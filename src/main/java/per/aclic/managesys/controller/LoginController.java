72acf5d57b4e08471f067ff7f3f7a65df19fc55c
uninstallHahaha <2234261505@qq.com>
2020-04-23 21:17:05 +0800

    记住密码完成

--- src/main/java/per/aclic/managesys/controller/LoginController.java
+++ src/main/java/per/aclic/managesys/controller/LoginController.java
@@ -8,6 +8,9 @@ import per.aclic.managesys.Utils.Utils;
 import per.aclic.managesys.model.User;
 import per.aclic.managesys.service.UserService;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,24 +23,43 @@ public class LoginController {
 
     @ResponseBody
     @RequestMapping("/signin")
-    public Map<String ,Object> signin(
+    public Map<String, Object> signin(
+            HttpServletRequest request,
+            HttpServletResponse response,
             HttpSession session,
-            String name, String pass){
+            String name, String pass, int mem) {
         HashMap<String, Object> map = new HashMap<>();
         User oneByName = userService.findOneByName(name);
-        if(null == oneByName){
-            map.put("stat",0);
-            map.put("data","账号不存在");
+        if (null == oneByName) {
+            map.put("stat", 0);
+            map.put("data", "账号不存在");
             return map;
-        }else{
-            if(!oneByName.getPass().equals(pass)){
-                map.put("stat",0);
-                map.put("data","密码不正确");
+        } else {
+            if (!oneByName.getPass().equals(pass)) {
+                map.put("stat", 0);
+                map.put("data", "密码不正确");
                 return map;
-            }else{
+            } else {
+                //cookie
+                Cookie nameCookie = new Cookie("name", name);
+                Cookie psCookie = new Cookie("pass", pass);
+                nameCookie.setPath(request.getContextPath() + "/");
+                psCookie.setPath(request.getContextPath() + "/");
+                if(mem == 1){
+                    nameCookie.setMaxAge(7*24*60*60);
+                    psCookie.setMaxAge(7*24*60*60);
+                    response.addCookie(nameCookie);
+                    response.addCookie(psCookie);
+                }else {
+                    nameCookie.setMaxAge(0);
+                    response.addCookie(nameCookie);
+                    psCookie.setMaxAge(0);
+                    response.addCookie(psCookie);
+                }
+                //session
                 session.setAttribute("USER", oneByName);
-                map.put("stat",1);
-                map.put("data","");
+                map.put("stat", 1);
+                map.put("data", "");
                 return map;
             }
         }
@@ -45,31 +67,31 @@ public class LoginController {
 
     @ResponseBody
     @RequestMapping("/signup")
-    public Map<String , Object> signup(HttpSession session,
-                                       String name ,String pass,int role){
+    public Map<String, Object> signup(HttpSession session,
+                                      String name, String pass, int role) {
         HashMap<String, Object> map = new HashMap<>();
         User oneByName = userService.findOneByName(name);
-        if(oneByName != null){
-            map.put("stat",0);
-            map.put("data","该账号已存在");
+        if (oneByName != null) {
+            map.put("stat", 0);
+            map.put("data", "该账号已存在");
             return map;
         }
-        User user = new User(Utils.genUUID(),name,pass,role);
+        User user = new User(Utils.genUUID(), name, pass, role);
         int addRes = userService.addUser(user);
-        if(addRes ==  1){
-            session.setAttribute("USER",user);
-            map.put("stat",1);
-            map.put("data","");
+        if (addRes == 1) {
+            session.setAttribute("USER", user);
+            map.put("stat", 1);
+            map.put("data", "");
             return map;
-        }else{
-            map.put("stat",0);
-            map.put("data","注册失败,服务器异常");
+        } else {
+            map.put("stat", 0);
+            map.put("data", "注册失败,服务器异常");
             return map;
         }
     }
 
     @RequestMapping("/logout")
-    public String logout(HttpSession session){
+    public String logout(HttpSession session) {
         session.invalidate();
         return "/mm/classic/mmenu/html/login/login";
     }
--- src/main/resources/static/xgather/xjs/signin.js
+++ src/main/resources/static/xgather/xjs/signin.js
@@ -2,6 +2,7 @@ $(function () {
     $('#signinbtn').click(function () {
         var name = $('#name').val()
         var pass = $('#pass').val()
+        var mem = $('input[name="rememberMe"]:checked').length
         if (name == "" || pass == "") {
             layer.msg('账号或密码不能为空')
             return
@@ -9,7 +10,7 @@ $(function () {
         $.ajax({
             url: '/signin',
             method:'post',
-            data: {name: name, pass: pass},
+            data: {name: name, pass: pass, mem:mem},
             success: function (res) {
                 if (res.stat == 1) {
                     layer.msg('登录成功')
@@ -19,5 +20,25 @@ $(function () {
                 }
             }
         })
+    })
+
+    $('#name').bind('change',function () {
+
+        var arrCookie = document.cookie.split(';')
+        var username
+        var passWord
+        for (var i = 0; i < arrCookie.length; i++) {
+            var arr = arrCookie[i].trim().split('=')
+            if (arr[0] === 'name') {
+                 username = arr[1]
+            }
+            if (arr[0] === 'pass') {
+                 passWord = arr[1]
+            }
+        }
+        if(username == $('#name').val()){
+            $('#pass').val(passWord)
+            $('#rememberMe').attr("checked", 'checked')
+        }
     })
 })
